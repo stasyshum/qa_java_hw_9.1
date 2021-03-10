33b0cf05cec1195818506feb5cdebe238e1aa804
Anastasia Shusmskaya <anastasiakoa@icloud.com>
2021-03-10 18:00:29 +0300

    +Task 2 PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, int domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
